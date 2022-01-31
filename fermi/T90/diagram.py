80e926e359c3048d7365b83044971245c83de385
mustaMg <60274926+mustaMg@users.noreply.github.com>
2022-01-31 19:12:55 +0300

    need some oprimizatation

--- fermi/T90/diagram.py
+++ fermi/T90/diagram.py
@@ -1,59 +1,50 @@
-#%%
-#https://stackoverflow.com/questions/48874733/fit-triple-gauss-to-data-python
-
-# https://stackoverflow.com/questions/15556930/turn-scatter-data-into-binned-data-with-errors-bars-equal-to-standard-deviation
-# yukaridaki linkte binlenmis datanin sigmasini almayi anlatiyor
 from astropy.io import fits         #reading fits files
 import matplotlib.pyplot as plt     #plotting the graph
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
 
-#hdul = fits.open('fermi_all.fits')    
+#hdul = fits.open('fermi/T90/fermi_all_error.fits')
 hdul = fits.open('fermi_all_error.fits')
-# our interested data is in table 1
 data = pd.DataFrame(hdul[1].data)
 
-# we are interested only with t90's
-#data = (data['T90'])
+# bin degerlerini olusturma
 bin = np.array(np.logspace(-2,3, num= 50), dtype=np.float64)
-x = np.array(data['T90'].to_list(), dtype=np.float64)
-error = np.array(data['T90_ERROR'].to_list(), dtype=np.float64)
-frequancy = ([len(data[data['T90'].between(bin[i-1], bin[i])]) for i in range(0, len(bin-1))])
-# frequancy plot ederken kullanilmiyor, amaci gauss fit cizerken sigma verisini elde etmek
+data = np.array(data['T90'].to_list(), dtype=np.float64)
 
-#   frequancy = ([len(data['T90'][(data['T90'] > bin[i]) & (data['T90'] < bin[i+1])]) for i in range(0, len(bin)-1)])
-# GAUSS FIT
-import pylab as plb
-from math import e
-from numpy import exp
-
-n = len(bin)                          #the number of data
-mean = sum(bin*frequancy)/n                   #note this correction
-sigma = sum(frequancy*(bin-mean)**2)/n        #note this correction
-
-def gaus(x,a,x0,sigma):
-    return a*exp(-(x-x0)**2/(2*sigma**2))
-
-popt,pcov = curve_fit(gaus,frequancy,bin,p0=[1,mean,sigma])
-# burada plt.hist degistirldi, hesaplamalari kolaylastirmak icin
 plt.figure()
-plt.hist(x, bins=bin, histtype='step')
-#plt.plot(bin,gaus(frequancy,*popt),'ro:',label='fit')
+
+freq= plt.hist(data, bins=bin, histtype='step', zorder=1)
+plt.xscale('log')
+ydata = np.append(freq[0], 0)
+xdata = freq[1]
+
+def gauss(x, H, A, x0, sigma):
+    return H + A * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+
+def gauss_fit(x, y):
+    mean = sum(x * y) / sum(y)
+    sigma = np.sqrt(sum(y * (x - mean) ** 2) / sum(y))
+    popt, pcov = curve_fit(gauss, x, y, p0=[min(y), max(y), mean, sigma])
+    return popt
 
 # for error plot
-y,binEdges = np.histogram(data['T90'],bins=bin)
-bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
-menStd     = np.sqrt(y)
-width      = 0.008
-plt.bar(x=bincenters, height = y, width=width, color='w', yerr=menStd)
-# color is white because i coulndt find a way with out plotting data
-#plt.legend()
-plt.gca().set_xscale("log")
-plt.xlabel("T90 sec.")
-plt.ylabel("Number of Burst")
-plt.show()
-#%%
+bincenters = 0.5*(xdata[1:]+xdata[:-1])
+menStd     = np.sqrt(ydata)
+width      = 0.001
+bincenters = np.append(bincenters, 0) # len of these data do not match, so I add zero to and
 
-# ok i understand it now 
-# I did it xd
+gauss_1 = gauss(xdata[:25], *gauss_fit(xdata[:25], ydata[:25]))
+gauss_2 = gauss(xdata, *gauss_fit(xdata, ydata))[24:]
+gauss_1[-1] = gauss_2[0]
+
+plt.plot(xdata[:25], gauss_1, 'r', label='fit', zorder=3)
+plt.plot(xdata[24:], gauss_2, 'r', label='fit', zorder=4)
+
+plt.bar(x=bincenters, height = ydata, width=width, color='w', yerr=menStd, zorder=2)
+# color is white because i coulndt find a way with out plotting data
+# plt.legend()
+# plt.gca().set_xscale("log")
+plt.xlabel('T90 sec')
+plt.ylabel('Number of Burst')
+plt.show()
