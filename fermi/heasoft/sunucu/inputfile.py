f32fab4dfe414e79c69e4417dbc1eb7f1755d680
mustaMg <60274926+mustaMg@users.noreply.github.com>
2022-02-15 13:50:27 +0300

    gonna use different scripts for ssh and local, created new folder called "sunucu"

---
+++ fermi/heasoft/sunucu/inputfile.py
@@ -0,0 +1,73 @@
+# in this file we are gonna use 10 different GRB events files
+# and heasoft to create some LC etc.
+import os
+import pandas as pd
+
+def get_path():
+    try: 
+        ask_path = input('Enter a path: ')
+        #given_path = '/Users/mustafagumustas/Downloads/Swift_BAT/bat_data'
+    except:
+        print('Path error!')
+    return ask_path
+
+if __name__ == '__main__':
+    # os.chdir("/Users/mustafagumustas/Downloads/Swift_BAT/bat_data")
+    # gonan use below later as main
+    given_path = get_path()
+    os.chdir(given_path)
+    
+# creating list of names 
+grbs = [i for i in os.listdir() if i != '.DS_Store']
+
+def onesec_4ch(grb, infile):
+    os.system(f'batbinevt {infile} {given_path}/{grb}/LC/onesec_4ch.lc LC 1.0 uniform 15-25,25-50,50-100,100-150 clobber=YES')
+    return 0
+
+def onesec(grb, infile):
+    os.system(f'batbinevt {infile} {given_path}/{grb}/LC/onesec.lc LC 1.0 uniform 15-150 clobber=YES')
+    return 0
+
+def msec64_4ch(grb, infile):
+    os.system(f'batbinevt {infile} {given_path}/{grb}/LC/msec64_4ch.lc LC 0.064 uniform 15-25,25-50,50-100,100-150 clobber=YES')
+    return 0
+
+def msec64(grb, infile):
+    os.system(f'batbinevt {infile} {given_path}/{grb}/LC/msec64.lc LC 0.064 uniform 15-150 clobber=YES')
+    return 0
+
+def unweighted_onesec(grb, infile):
+    os.system(f'batbinevt infile={infile} outfile={given_path}/{grb}/LC/total.dpi \
+        outtype=DPI timedel=1.0 timebinalg=u energybins=15-25,25-50,50-100,100-150 \
+        weighted=NO outunits=COUNTS clobber=YES')
+
+
+def lc_creator(grb):
+    try:
+        os.chdir(f'{given_path}/{grb}')
+        #os.system('mkdir LC')
+        file = os.listdir()
+        # There might be errors in future, diff os systems creates diff names!!!
+        file = [i for i in file if i != '.DS_Store'][0]
+        os.chdir(given_path+f'/{grb}/{file}/bat/event')
+        event_files = os.listdir()
+        infile = [i for i in event_files if 'bevshsp' in i][0]
+        onesec_4ch(grb, infile)
+        #onesec(grb, infile)
+        #msec64_4ch(grb, infile)
+        msec64(grb, infile)
+        unweighted_onesec(grb,infile)
+        return infile
+
+    except:
+        print(f'Error, {given_path}/{grb}/{file}/bat/event check if this dir exists')
+        return 1
+
+
+names = pd.DataFrame([lc_creator(grb) for grb in grbs])
+
+#names.to_csv('/nextq/mustafa//deneme.txt', sep='\t') # for ssh
+#names.to_csv('/Volumes/GoogleDrive/My\ Drive/Python/Fermi/fermi/heasoft/deneme.txt', sep='\t') # local
+
+# /Swift_BAT/bat_data/grb200303A/00959431000/bat/event
+# /Users/mustafagumustas/Downloads/Swift_BAT/bat_data
