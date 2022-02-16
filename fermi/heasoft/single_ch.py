b160069854b05d6cb15583a90b9e91e63926c5ce
mustaMg <60274926+mustaMg@users.noreply.github.com>
2022-02-16 14:58:34 +0300

    added first criteria, before code was not detecting if long grb that fixed either

--- fermi/heasoft/single_ch.py
+++ fermi/heasoft/single_ch.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.rcParams['figure.dpi'] = 200
+
 hdul = fits.open('msec64.lc')
 
 # need to create energy ranges to give column names
@@ -14,21 +15,49 @@ e_max = (pd.DataFrame(hdul[2].data))['E_MAX'].to_list()
 e_max = ([int(i) for i in e_max])
 column = [f'{e_min[i]}-{e_max[i]}'for i in range(len(e_min))]
 
-# time data
+# TIME DATA
 time = pd.DataFrame([hdul[1].data[i][0] for i in range(len(hdul[1].data))])
 TRIGTIME = hdul[1].header['TRIGTIME']
-# we have to substracy trig time from all time values to get 0 at real trigger time
+# we have to substracy trig time from all time values to get 0 at
+# real trigger time
 time = time - TRIGTIME
 
-# count data
-count = pd.DataFrame([hdul[1].data[i][1] for i in range(len(hdul[1].data))], columns=column, index=time[0])
+# COUNT DATA
+count = pd.DataFrame([hdul[1].data[i][1] for i in range(len(hdul[1].data))],\
+    columns=column, index=time[0])
 NGOODPIX = hdul[1].header['NGOODPIX']
 # we have to multiply count values with NGOODPIX to get real count numbers
 count = count * NGOODPIX
 
+# ERROR DATA
 # error = pd.DataFrame([hdul[1].data[i][2] for i in range(len(hdul[1].data))])
 
-###################################################################################################
+# between -1 and 5th sec
+sec5_count = count[(-1 < count.index) & (count.index < 5)] 
+max_c = max(sec5_count['15-150'])   # max photon count value
+
+max_i = sec5_count.loc[sec5_count['15-150'] == max_c].index[0]
+
+# FIRST MORPHOLOGICAL CRITERIA
+# if the max count rate after the fifth second thats a long GRB
+while True:
+    if max_i < 5:
+        break 
+    else:
+        exit()
+
+
+# SECOND MORPHOLOGICAL CRITERIA
+# if count rate is below 11k look under %40 else %30
+thrty_p = max_c * 0.4 if max_c < 11000 else max_c * 0.3
+
+#  data set tht ones after %30 count values
+prcnt30_count = sec5_count[max_i < sec5_count.index]
+
+params = prcnt30_count['15-150']-thrty_p
+params = (params.abs()).sort_values()
+
+################################################################################
 # plotting begins
 
 f, (ax1, ax2) = plt.subplots(2,1 ,sharex=False, sharey=False)
@@ -37,25 +66,6 @@ ax1.set_xlim([-100,350])
 ax1.axvline(x = 5, color='b', linestyle='--', label='5.th sec')
 ax1.set_ylabel('Count rate/sec')
 
-# data between -1 and 5th sec
-sec5_count = count[(-1 < count.index) & (count.index < 5)] 
-max_c = max(sec5_count['15-150'])   # max photon count value
-
-# SECOND MORPHOLOGICAL CRITERIA
-# if count rate is below 11k look under %40 else %30
-thrty_p = max_c * 0.4 if max_c < 11000 else max_c * 0.3
-
-max_i = [i for i, ct in zip(sec5_count.index, sec5_count['15-150']) if ct == max_c][0]
-
-
-
-prcnt30_count = sec5_count[max_i < sec5_count.index]
-# above data set is the ones after %30 count values
-print(max_i)
-params = prcnt30_count['15-150']-thrty_p
-params = (params.abs()).sort_values()
-print(params)
-#%%
 # ax2 is zoomed one
 ax2.plot(count['15-150'])
 ax2.set_xlim([-5,10])
--- fermi/heasoft/sunucu/single_ch_ssh.py
+++ fermi/heasoft/sunucu/single_ch_ssh.py
@@ -33,56 +33,69 @@ def single_ch(grb, file_name):
     e_max = ([int(i) for i in e_max])
     column = [f'{e_min[i]}-{e_max[i]}'for i in range(len(e_min))]
 
-    # time data
+    # TIME DATA
     time = pd.DataFrame([hdul[1].data[i][0] for i in range(len(hdul[1].data))])
     TRIGTIME = hdul[1].header['TRIGTIME']
-    # we have to substracy trig time from all time values to get 0 at real trigger time
+    # we have to substracy trig time from all time values to get 0 at 
+    # real trigger time
     time = time - TRIGTIME
 
     # count data
-    count = pd.DataFrame([hdul[1].data[i][1] for i in range(len(hdul[1].data))], columns=column, index=time[0])
+    count = pd.DataFrame([hdul[1].data[i][1] for i in range(len(hdul[1].data))],\
+        columns=column, index=time[0])
     NGOODPIX = hdul[1].header['NGOODPIX']
     # we have to multiply count values with NGOODPIX to get real count numbers
     count = count * NGOODPIX
 
+    # ERROR DATA
     # error = pd.DataFrame([hdul[1].data[i][2] for i in range(len(hdul[1].data))])
 
-    ###################################################################################################
-    # plotting begins
+    # between -1 and 5th sec
+    
+    max_c = max(count['15-150'])   # max photon count value
+    max_i = count.loc[count['15-150'] == max_c].index[0]
 
-    f, (ax1, ax2) = plt.subplots(2,1 ,sharex=False, sharey=False)
-    ax1.set_title(f'{grb}')
-    ax1.plot(count['15-150'])
-    ax1.set_xlim([-100,350])
-    ax1.axvline(x = 5, color='b', linestyle='--', label='5.th sec')
-    ax1.set_ylabel('Count rate/sec')
+    # FIRST MORPHOLOGICAL CRITERIA
+    # if the max count rate after the fifth second thats a long GRB
+    if max_i < 5:
+        sec5_count = count[(-1 < count.index) & (count.index < 5)] 
+        max_c = max(sec5_count['15-150'])   # max photon count value between
+        # max and fifth sec
+        # SECOND MORPHOLOGICAL CRITERIA
+        # if count rate is below 11k look under %40 else %30
+        thrty_p = max_c * 0.4 if max_c < 11000 else max_c * 0.3
 
-    # data between -1 and 5th sec
-    sec5_count = count[(-1 < count.index) & (count.index < 5)] 
-    max_c = max(sec5_count['15-150'])   # max photon count value
+        # data set tht ones after %30 count values
+        prcnt30_count = sec5_count[max_i < sec5_count.index]
 
-    # SECOND MORPHOLOGICAL CRITERIA
-    # if count rate is below 11k look under %40 else %30
-    thrty_p = max_c * 0.4 if max_c < 11000 else max_c * 0.3
+        params = prcnt30_count['15-150']-thrty_p
+        params = (params.abs()).sort_values()
 
-    max_i = [i for i, ct in zip(sec5_count.index, sec5_count['15-150']) if ct == max_c][0]
+        ############################################################################
+        # plotting begins
+        
+        f, (ax1, ax2) = plt.subplots(2,1 ,sharex=False, sharey=False)
+        ax1.set_title(f'{grb[:3].upper()} {grb[3:]}')
+        ax1.plot(count['15-150'])
+        ax1.set_xlim([-100,350])
+        ax1.axvline(x = 5, color='b', linestyle='--', label='5.th sec')
+        ax1.set_ylabel('Count rate/sec')
 
-    prcnt30_count = sec5_count[max_i < sec5_count.index]
-    params = prcnt30_count['15-150']-thrty_p
-    params = (params.abs()).sort_values()
+        # ax2 is zoomed one
+        ax2.plot(count['15-150'])
+        ax2.set_xlim([-5,10])
+        ax2.axhline(y = thrty_p, color='r')
+        ax2.axhline(y = 0, color='r', linestyle='--')
+        ax2.axvline(x = 5, color='b', linestyle='--')
+        ax2.axvline(x = params.index[0], color='g', linestyle='--')
+        ax2.set_ylabel('Count rate/sec')
+        ax2.set_xlabel('Time since the trigger (sec)')
+        print(f'{given_path}/{grb}/LC/{file_name}.pdf')
+        # plt.savefig(f'{given_path}/{grb}/LC/{file_name}.pdf')
+    else:
+        quit
+        return 1
 
-    # ax2 is zoomed one
-    ax2.plot(count['15-150'])
-    ax2.set_xlim([-5,10])
-    ax2.axhline(y = thrty_p, color='r')
-    ax2.axhline(y = 0, color='r', linestyle='--')
-    ax2.axvline(x = 5, color='b', linestyle='--')
-    ax2.axvline(x = params.index[0], color='g', linestyle='--')
-    ax2.set_ylabel('Count rate/sec')
-    ax2.set_xlabel('Time since the trigger (sec)')
-
-    print(f'{given_path}/{grb}/LC/{file_name}.pdf')
-    # plt.savefig(f'{given_path}/{grb}/LC/{file_name}.pdf')
 
 
 def lc_picker(grb):
